acc2b66890ed722457234dad9a5c59ec45c46024
colkito <github@colkito.com>
2023-02-05 20:20:30 -0300

    update image src

--- pages/example.tsx
+++ pages/example.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import Link from 'next/link'
+import Link from '@/components/IPFSLink'
 import styles from '@/styles/Home.module.css'
 
 export default function Example() {
