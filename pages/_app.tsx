0fb312bf58babc365d92769221a05230a9f3d6fd
colkito <github@colkito.com>
2023-02-05 18:43:12 -0300

    add IPFS support for base href

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
+import Script from 'next/script'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script id="ipfs-base-href">
+        {`
+(function () {
+  const { pathname } = window.location
+  const ipfsMatch = /.*\\/Qm\\w{44}\\//.exec(pathname)
+  const base = document.createElement('base')
+  base.href = ipfsMatch ? ipfsMatch[0] : '/'
+  document.head.append(base)
+})();
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  )
 }
