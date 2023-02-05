f2aed68556a6d28e530ae9aa8228fa9df0e84453
colkito <github@colkito.com>
2023-02-05 18:56:05 -0300

    fix app script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
         {`
 (function () {
   const { pathname } = window.location
-  const ipfsMatch = /.*\\/Qm\\w{44}\\//.exec(pathname)
+  const ipfsMatch =/^\/ipfs\/(Qm[a-zA-Z0-9]{44})/.exec(pathname)
   const base = document.createElement('base')
   base.href = ipfsMatch ? ipfsMatch[0] : '/'
   document.head.append(base)
