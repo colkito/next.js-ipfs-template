46ab667188986230d3fa1142e68fd033b1d56398
colkito <github@colkito.com>
2023-02-05 20:55:56 -0300

    add CSP support

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,24 @@
+import crypto from 'crypto'
+import { HtmlProps } from 'next/dist/shared/lib/html-context'
 import { Html, Head, Main, NextScript } from 'next/document'
 
-export default function Document() {
+const cspHashOf = (text: string) => {
+  const hash = crypto.createHash('sha256')
+  hash.update(text)
+  return `'sha256-${hash.digest('base64')}'`
+}
+
+export default function Document(ctx: HtmlProps) {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content={`default-src 'self'; script-src 'self' ${cspHashOf(
+            NextScript.getInlineScriptSource(ctx)
+          )}`}
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
