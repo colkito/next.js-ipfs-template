4430bce44201f18a398deb39beb2eabf3c3eeb53
colkito <github@colkito.com>
2023-02-05 20:33:35 -0300

    update Script for base href

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,17 @@ import type { AppProps } from 'next/app'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script id="ipfs-base-href">
-        {`
-  const { pathname } = window.location
-  const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
-  const ipfsMatch = ipfsRegex.exec(pathname)
-  const base = document.createElement('base')
-  base.href = ipfsMatch ? ipfsMatch[0] : '/'
-  document.head.append(base)
-        `}
-      </Script>
+      <Script
+        id="ipfs-base-href"
+        dangerouslySetInnerHTML={{
+          __html: `const { pathname } = window.location
+    const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
+    const ipfsMatch = ipfsRegex.exec(pathname)
+    const base = document.createElement('base')
+    base.href = ipfsMatch ? ipfsMatch[0] : '/'
+    document.head.append(base)`,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
