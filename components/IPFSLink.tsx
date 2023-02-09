93065e4604b864404f1df757579d60f7b90ba997
colkito <github@colkito.com>
2023-02-08 23:53:40 -0300

    update ipfs base script

--- components/IPFSLink.tsx
+++ components/IPFSLink.tsx
@@ -1,5 +1,5 @@
-import Link, { LinkProps } from 'next/link'
 import { useMemo } from 'react'
+import Link, { LinkProps } from 'next/link'
 
 export type IPFSLinkProps = LinkProps & {
   children?: React.ReactNode
@@ -11,25 +11,38 @@ export type IPFSLinkProps = LinkProps & {
 // based on https://github.com/Velenir/nextjs-ipfs-example#deploying-nextjs-site-to-ipfs
 const IPFSLink = ({ href, as, ...rest }: IPFSLinkProps) => {
   const newAs = useMemo(() => {
-    const asHref = (as || href) as string
+    let baseAsHref = (as ?? href) as string
 
-    if (asHref.startsWith('/') && typeof document !== 'undefined') {
-      const { pathname } = document.location
-      const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
-      const ipfsMatch = ipfsRegex.exec(pathname)
+    // make absolute url relative when displayed in url bar
+    if (baseAsHref.startsWith('/')) {
+      //  for static html compilation
+      baseAsHref = '.' + href
+      // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
 
-      if (ipfsMatch) {
-        const slashRegex = new RegExp('/$')
-        const ipfsPathname = ipfsMatch[0].replace(slashRegex, '')
-        return `${ipfsPathname}${asHref}`
-        // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="/ipfs/Qm<hash>/about">About</a>
-      }
+      // document is unavailable when compiling on the server
+      // if (typeof document !== 'undefined') {
+      //   const { pathname } = document.location
+
+      //   console.log('pathname', pathname)
+      //   console.log('baseURI', document.baseURI)
+
+      //   // const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
+      //   // const ipfsMatch = ipfsRegex.exec(pathname)
+
+      //   // if (ipfsMatch) {
+      //   //   const slashRegex = new RegExp('/$')
+      //   //   const ipfsPathname = ipfsMatch[0].replace(slashRegex, '')
+      //   //   return `${ipfsPathname}${baseAsHref}`
+      //   //   // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="/ipfs/Qm<hash>/about">About</a>
+      //   // }
+      // }
+
     }
 
-    return asHref
+    return baseAsHref
   }, [as, href])
 
-  return <Link {...rest} href={newAs} />
+  return <Link {...rest} href={href} as={newAs} />
 }
 
 export default IPFSLink
--- public/ipfsBaseHref.js
+++ public/ipfsBaseHref.js
@@ -1,9 +1,9 @@
 ;(() => {
+  const defaultBaseHref = '/'
   const { pathname } = window.location
   const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
   const ipfsMatch = ipfsRegex.exec(pathname)
   const base = document.createElement('base')
-  base.href = ipfsMatch ? ipfsMatch[0] : '/'
+  base.href = ipfsMatch ? ipfsMatch[0] : defaultBaseHref
   document.head.append(base)
-  console.log('This module has been auto-called!', base.href)
 })()
