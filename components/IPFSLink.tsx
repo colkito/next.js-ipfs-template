15cd0386ee289ddad52fa979c33905dff7bcaacf
colkito <github@colkito.com>
2023-02-08 23:59:42 -0300

    back link component code

--- components/IPFSLink.tsx
+++ components/IPFSLink.tsx
@@ -1,48 +1,28 @@
-import { useMemo } from 'react'
 import Link, { LinkProps } from 'next/link'
+import { useMemo } from 'react'
+import {resolve} from 'url'
 
-export type IPFSLinkProps = LinkProps & {
-  children?: React.ReactNode
-  className?: string
-  target?: string
-  rel?: string
-}
+const IPFSLink = ({href, as, ...rest}: LinkProps) => {
 
-// based on https://github.com/Velenir/nextjs-ipfs-example#deploying-nextjs-site-to-ipfs
-const IPFSLink = ({ href, as, ...rest }: IPFSLinkProps) => {
   const newAs = useMemo(() => {
-    let baseAsHref = (as ?? href) as string
+    let baseURI_as = (as || href) as string
 
-    // make absolute url relative when displayed in url bar
-    if (baseAsHref.startsWith('/')) {
+    // make absolute url relative
+    // when displayed in url bar
+    if (baseURI_as.startsWith('/')) {
       //  for static html compilation
-      baseAsHref = '.' + href
+      baseURI_as = '.' + href
       // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="./about">About</a>
 
-      // document is unavailable when compiling on the server
-      // if (typeof document !== 'undefined') {
-      //   const { pathname } = document.location
-
-      //   console.log('pathname', pathname)
-      //   console.log('baseURI', document.baseURI)
-
-      //   // const ipfsRegex = new RegExp('^/ipfs/(Qm[a-zA-Z0-9]{44})')
-      //   // const ipfsMatch = ipfsRegex.exec(pathname)
-
-      //   // if (ipfsMatch) {
-      //   //   const slashRegex = new RegExp('/$')
-      //   //   const ipfsPathname = ipfsMatch[0].replace(slashRegex, '')
-      //   //   return `${ipfsPathname}${baseAsHref}`
-      //   //   // <IPFSLink href="/about"> => <a class="jsx-2055897931" href="/ipfs/Qm<hash>/about">About</a>
-      //   // }
-      // }
-
+      // on the client
+    //   document is unavailable when compiling on the server
+      if (typeof document !== 'undefined') {
+        baseURI_as = resolve(document.baseURI, baseURI_as)
+        // => <a href="https://gateway.ipfs.io/ipfs/Qm<hash>/about">About</a>
+      }
     }
-
-    return baseAsHref
+    return baseURI_as
   }, [as, href])
 
   return <Link {...rest} href={href} as={newAs} />
 }
-
-export default IPFSLink
